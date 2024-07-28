fd6100c4624b24d840fdbdee0a74b0770d20dfe0
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-07-28 20:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
 
+	let form = document.getElementById('form1')
+
+	let firstName = form.elements['fname'].value;
+	let lastName = form.elements['lname'].value;
+
+	alert(firstName + ' ' + lastName);
 }
